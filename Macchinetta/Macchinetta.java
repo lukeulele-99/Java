37a4d241cc99ac8bd769a2509f6c05356c061c45
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-05-06 15:16:42 +0200

    Aggiunta UX macchinetta

--- Macchinetta/Macchinetta.java
+++ Macchinetta/Macchinetta.java
@@ -30,8 +30,7 @@ public class Macchinetta {
             sc.nextLine(); // pulizia buffer
 
             if (nome.equals(u.getNome()) && password == Integer.parseInt(u.getPassword())) {
-
-
+                m.mostraProdotti();
             } else {
                 System.out.println("Credenziali errate.");
             }
@@ -44,67 +43,28 @@ public class Macchinetta {
 
             if (idAdmin == 1000) {
                 System.out.println("Accesso Admin riuscito.");
-                m.mostraMenuAdmin(sc);
+
             } else {
                 System.out.println("ID Admin errato.");
             }
         }
     }
 
-    public void mostraMenuAdmin(Scanner sc) {
-        boolean continua = true;
-
-        while (true) {
-            System.out.println("=== Menu Admin ===");
-            System.out.println("1. Mostra tutti i prodotti");
-            System.out.println("2. Ricarica");
-            System.out.println("3. Aggiungi prodotto");
-            System.out.println("4. Modifica prodotto");
-            System.out.println("5. Rimuovi prodotto");
-            System.out.println("6. Torna indietro al menu principale");
-
-            String scelta = sc.nextLine();
-
-            switch (scelta) {
-                case "1":
-                    System.out.println("mostraProdotti()");
-                    break;
-                case "2":
-                    System.out.println("ricarica(sc)");
-                    break;
-                case "3":
-                    System.out.println("aggiungiProdotto");
-                    break;
-                case "4":
-                    System.out.println("modificaProdotto");
-                    break;
-                case "5":
-                    System.out.println("rimuoviProdotto");
-                    break;
-                default:
-                    System.out.println("Scelta non valida.");
+    public static void mostraProdotti() {
+        // Gruppo i prodotti per categoria
+        for (Prodotto.Categoria categoria : Prodotto.Categoria.values()) {
+            System.out.println("Categoria: " + categoria);
+            for (Prodotto prodotto : Prodotto.values()) {
+                if (prodotto.getCategoria() == categoria) {
+                    System.out.println(prodotto.getCodice() + " - " + prodotto.getNome());
+                }
             }
+            System.out.println(); // Separatore tra categorie
         }
     }
-
-    private void rimuoviProdotto(Scanner sc) {
-    }
-
-    private void modificaProdotto(Scanner sc) {
-    }
-
-    private void aggiungiProdotto(Scanner sc) {
-    }
-
-    private void ricarica(Scanner sc) {
-    }
-
-    private void mostraProdotti() {
-
-    }
-
-
 }
+
+
 
 
 
